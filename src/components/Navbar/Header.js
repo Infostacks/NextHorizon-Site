440874163b17ac709a895d228386d8beabcf1c54
Faaiz Yasha <faaiz.yasha@gmail.com>
2022-09-15 16:41:54 -0400

    initial commit

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -65,7 +65,7 @@ const Header = () => {
                   <span className={`text-sm font-medium`}>
                     <NavLink
                       to="/"
-                      className={`tracking-widest ${"active" ? "" : "navlinkUnderline"} `}
+                      className={`tracking-widest hover:navlinkUnderline active:notnavlinkUnderline`}
                     >
                       Home
                     </NavLink>

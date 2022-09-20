87838c924eaa754cd1d56ee8f30c9966330fa8ca
Muhammad Asif <asif88383@gmail.com>
2022-09-20 13:17:41 -0700

    get a quote page animation added

--- src/components/Contact/AboutProject.js
+++ src/components/Contact/AboutProject.js
@@ -1,40 +1,72 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 const AboutProject = () => {
+  const { ref, inView } = useInView({
+    threshold: [0, 0.5, 1.0],
+  });
+  const upAnimation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      upAnimation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 0.5,
+        },
+      });
+    }
+
+    if (!inView) {
+      upAnimation.start({ y: "5vw", opacity: 0 });
+    }
+  }, [inView, upAnimation]);
+
   return (
-    <div className="flex xl:flex-row lg:flex-row flex-col justify-around gap-5 py-5 xl:px-16 lg:px-16 md:px-10 px-5 rounded-[3rem] bg-gradient-to-bl from-rose-50 via-rose-100 to-rose-300 xl:w-4/6 lg:w-4/6 w-5/6">
+    <div
+      ref={ref}
+      className="flex xl:flex-row lg:flex-row flex-col justify-around gap-5 py-5 xl:px-16 lg:px-16 md:px-10 px-5 rounded-[3rem] bg-gradient-to-bl from-rose-50 via-rose-100 to-rose-300 xl:w-4/6 lg:w-4/6 w-5/6"
+    >
       {/* left side  */}
       <div className="flex flex-col justify-evenly gap-10 xl:w-1/2 lg:w-1/2 w-full">
-        <h1 className="xl:text-5xl lg:text-4xl text-2xl font-extrabold">
+        <motion.h1
+          animate={upAnimation}
+          className="xl:text-5xl lg:text-4xl text-2xl font-extrabold"
+        >
           Tell Us About Your Project
-        </h1>
-        <p>
+        </motion.h1>
+        <motion.p animate={upAnimation}>
           We’ll contact you within a couple of hours to schedule a meeting to
           discuss your goals.
-        </p>
+        </motion.p>
 
         <div className="flex xl:flex-row lg:flex-row md:flex-row flex-col gap-5">
-          <div className="flex flex-col">
+          <motion.div animate={upAnimation} className="flex flex-col">
             <div className="flex flex-row gap-2">
               <span>icon</span>
               <h3 className="font-semibold">Clutch</h3>
             </div>
             <span>Reviews 51 . Rating 4.9/5</span>
-          </div>
-          <div className="flex flex-col">
+          </motion.div>
+
+          <motion.div animate={upAnimation} className="flex flex-col">
             <div className="flex flex-row gap-2">
               <span>icon</span>
               <h3 className="font-semibold">GoodFirms</h3>
             </div>
             <span>Reviews 51 . Rating 4.9/5</span>
-          </div>
+          </motion.div>
         </div>
       </div>
 
       {/* right side  */}
       <form className="flex flex-col justify-start xl:w-1/2 lg:w-1/2 w-full gap-5">
-        <div className="flex xl:flex-row lg:flex-row flex-col gap-10">
+        <motion.div
+          animate={upAnimation}
+          className="flex xl:flex-row lg:flex-row flex-col gap-10"
+        >
           <input
             type="text"
             name="name"
@@ -47,8 +79,11 @@ const AboutProject = () => {
             placeholder="Company"
             className="ext-gray-600 font-light p-2 border-b-[1px] border-gray-400 focus:outline-none bg-transparent font-xs w-full"
           />
-        </div>
-        <div className="flex xl:flex-row lg:flex-row flex-col gap-10">
+        </motion.div>
+        <motion.div
+          animate={upAnimation}
+          className="flex xl:flex-row lg:flex-row flex-col gap-10"
+        >
           <input
             type="email"
             name="email"
@@ -61,31 +96,43 @@ const AboutProject = () => {
             placeholder="Your phone number"
             className="ext-gray-600 font-light p-2 border-b-[1px] border-gray-400 focus:outline-none bg-transparent font-xs w-full"
           />
-        </div>
+        </motion.div>
 
-        <div className="flex xl:flex-row lg:flex-row flex-col gap-10">
+        <motion.div
+          animate={upAnimation}
+          className="flex xl:flex-row lg:flex-row flex-col gap-10"
+        >
           <input
             type="text"
             name="aboutProject"
             placeholder="About a project *"
             className="ext-gray-600 font-light p-2 border-b-[1px] border-gray-400 focus:outline-none bg-transparent font-xs w-full"
           />
-        </div>
+        </motion.div>
 
-        <div className="flex xl:flex-row lg:flex-row flex-col gap-10">
+        <motion.div
+          animate={upAnimation}
+          className="flex xl:flex-row lg:flex-row flex-col gap-10"
+        >
           <input
             type="file"
             name="file"
             className="uploadBtn text-gray-600 font-light border-b-[1px] border-gray-400 bg-transparent font-xs w-full"
           />
-        </div>
+        </motion.div>
 
-        <div className="flex flex-row items-center justify-start gap-2">
+        <motion.div
+          animate={upAnimation}
+          className="flex flex-row items-center justify-start gap-2"
+        >
           <input type="checkbox" name="nda" className="bg-transparent" />
           <span>Send me NDA</span>
-        </div>
+        </motion.div>
 
-        <div className="flex xl:flex-row lg:flex-row flex-col items-center justify-start gap-2">
+        <motion.div
+          animate={upAnimation}
+          className="flex xl:flex-row lg:flex-row flex-col items-center justify-start gap-2"
+        >
           <span className="text-slate-500 xl:w-10/12 lg:w-10/12">
             <span>
               By sending this form I confirm that I have read and accept the
@@ -101,7 +148,7 @@ const AboutProject = () => {
           <button className="hover:bg-[#08080cff] text-[#252627] py-2 px-3 rounded-full drop-shadow-md hover:shadow-inner hover:bg-white hover:text-white border-[1px] hover:border-[#08080cff] border-[#D90429] w-[11rem]">
             Send Message
           </button>
-        </div>
+        </motion.div>
       </form>
     </div>
   );
--- src/components/Contact/OurOffices.js
+++ src/components/Contact/OurOffices.js
@@ -1,28 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import { offices } from "../../utils/data.js";
 
 const OurOffices = () => {
+  const { ref, inView } = useInView({
+    threshold: [0, 0.5, 1.0],
+  });
+  const fadeInAnimation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      fadeInAnimation.start({
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 1,
+        },
+      });
+    }
+
+    if (!inView) {
+      fadeInAnimation.start({ opacity: 0 });
+    }
+  }, [inView, fadeInAnimation]);
+
   return (
-    <section className="bg-slate-100 w-screen flex flex-col gap-10 items-center pb-20 overflow-x-hidden">
+    <section
+      ref={ref}
+      className="bg-slate-100 w-screen flex flex-col gap-10 items-center pb-20 overflow-x-hidden"
+    >
       {/* top section  */}
       <div className="max-w-screen-lg flex items-center px-10 xl:flex-row lg:flex-row flex-col xl:gap-20 lg:gap-14 gap-10">
         {/* Text intro  */}
-        <h2 className="text-4xl font-bold tracking-tight sm:text-5xl">
+        <motion.h2
+          animate={fadeInAnimation}
+          className="text-4xl font-bold tracking-tight sm:text-5xl"
+        >
           Our Offices
-        </h2>
+        </motion.h2>
 
-        <p className="max-w-lg mt-4 text-xl">
+        <motion.p animate={fadeInAnimation} className="max-w-lg mt-4 text-xl">
           With several offices in Ukraine and beyond, you’re more than welcome
           to come to visit us at any convenient time and meet your team in
           person.
-        </p>
+        </motion.p>
       </div>
 
       {/* awards  */}
       <div className="max-w-screen-lg grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 w-full px-10 gap-10">
         {offices.map((office, index) => {
           return (
-            <div key={index} className="flex flex-col gap-2">
+            <motion.div
+              initial={{ y: index % 2 === 0 ? "-10vh" : "10vh", opacity: 0 }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 2,
+                  type: "spring",
+                  bounce: 0.4,
+                },
+              }}
+              key={index}
+              className="flex flex-col gap-2"
+            >
               <img src={office.flag} alt="" className="w-16 h-10" />
 
               <div className="flex flex-col">
@@ -38,7 +80,7 @@ const OurOffices = () => {
 
                 <span className="text-xl text-gray-500">{office.contact}</span>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>

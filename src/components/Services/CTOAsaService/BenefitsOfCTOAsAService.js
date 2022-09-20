7f161546a303a16fd5b4cdb39829150fe242fb14
Muhammad Asif <asif88383@gmail.com>
2022-09-20 08:34:10 -0700

    CTO AS A SERVICE page animations added

--- src/components/Services/CTOAsaService/BenefitsOfCTOAsAService.js
+++ src/components/Services/CTOAsaService/BenefitsOfCTOAsAService.js
@@ -1,29 +1,82 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from "react";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import { benefitsCtoAsAService } from "../../../utils/data.js";
 
 const BenefitsOfCTOAsAService = () => {
-  return (
-    <section className="bg-slate-100 w-screen flex flex-col gap-10 items-center pt-20 overflow-x-hidden xl:px-0 lg:px-0 px-10">
-      {/* top section  */}
-      <div className="max-w-screen-lg flex xl:flex-row lg:flex-row flex-col gap-10 items-center">
-        {/* Text intro  */}
-        <span className="xl:text-7xl lg:text-7xl md:text-4xl text-3xl font-bold tracking-tight">
-          Benefits of CTO as a Service
-        </span>
+  const { ref, inView } = useInView({
+    threshold: [0, 0.5, 1.0],
+  });
+  const upAnimation = useAnimation();
+  const downAnimation = useAnimation();
+  const fadeInAnimation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      upAnimation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 0.5,
+        },
+      });
 
-        <span className="max-w-lg mt-4 xl:text-xl lg:text-xl text-base tracking-wide font-semibold">
+      downAnimation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 0.5,
+        },
+      });
+      fadeInAnimation.start({
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 1,
+        },
+      });
+    }
+
+    if (!inView) {
+      upAnimation.start({ y: "5vw", opacity: 0 });
+      downAnimation.start({ y: "-5vw", opacity: 0 });
+      fadeInAnimation.start({ opacity: 0 });
+    }
+  }, [inView, upAnimation, downAnimation, fadeInAnimation]);
+
+  return (
+    <section
+      ref={ref}
+      className="bg-slate-100 w-screen flex flex-col gap-10 items-center pt-20 pb-2 overflow-x-hidden xl:px-0 lg:px-0 px-10"
+    >
+      {/* top section  */}
+      <div className="max-w-screen-lg flex xl:flex-row lg:flex-row flex-col xl:gap-0 lg:gap-0 gap-10 items-center">
+        {/* Text intro  */}
+        <motion.span
+          animate={upAnimation}
+          className="xl:text-7xl lg:text-7xl md:text-4xl text-3xl font-bold tracking-tight"
+        >
+          Benefits of CTO as a Service
+        </motion.span>
+
+        <motion.span
+          animate={downAnimation}
+          className="max-w-lg mt-4 xl:text-xl lg:text-xl text-base tracking-wide font-semibold"
+        >
           Our tech executives effectively manage development teams, plan
           cost-effective development strategies, and comply with all clients'
           needs
-        </span>
+        </motion.span>
       </div>
 
       {/*   */}
       <div className="max-w-screen-lg flex flex-col w-full gap-2 bg-slate-200 rounded-3xl">
         {benefitsCtoAsAService.map((benefit, index) => {
           return (
-            <div
+            <motion.div
+              animate={fadeInAnimation}
               className="flex flex-col justify-center bg-slate-50 xl:p-10 lg:p-10 p-5 rounded-3xl drop-shadow-md"
               key={index}
             >
@@ -35,7 +88,7 @@ const BenefitsOfCTOAsAService = () => {
                   {benefit.desc}
                 </span>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
--- src/components/Services/CTOAsaService/CTODevInsights.js
+++ src/components/Services/CTOAsaService/CTODevInsights.js
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import { Link } from "react-router-dom";
 import { ctoDevInsights } from "../../../utils/data.js";
 
 const CTODevInsights = () => {
+  const { ref, inView } = useInView({
+    threshold: [0, 0.5, 1.0],
+  });
+  const upAnimation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      upAnimation.start({
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 2,
+          bounce: 0.5,
+        },
+      });
+    }
+
+    if (!inView) {
+      upAnimation.start({ y: "5vw", opacity: 0 });
+    }
+  }, [inView, upAnimation]);
+
   return (
-    <section className="bg-slate-100 w-screen flex flex-col gap-10 items-center py-20 overflow-x-hidden xl:px-0 lg:px-0 px-10">
+    <section
+      ref={ref}
+      className="bg-slate-100 w-screen flex flex-col gap-10 items-center py-20 overflow-x-hidden xl:px-0 lg:px-0 px-10"
+    >
       {/* top section  */}
       <div className="max-w-screen-xl flex items-center xl:px-10 xl:flex-row lg:flex-row flex-col gap-20">
         {/* Text intro  */}
-        <div className="xl:text-7xl lg:text-7xl md:text-4xl text-3xl font-bold tracking-wider antialiased">
-          CTO as a Service insights
-        </div>
+        <motion.div
+          animate={upAnimation}
+          className="xl:text-7xl lg:text-7xl md:text-4xl text-3xl font-bold tracking-wider antialiased"
+        >
+          CTO as a Service Insights
+        </motion.div>
       </div>
 
       {/*   */}
       <div className="flex flex-row justify-center flex-wrap gap-5">
         {ctoDevInsights.map((insight, index) => {
           return (
-            <div
+            <motion.div
+              animate={upAnimation}
               key={index}
-              className={`flex flex-col justify-between gap-3 xl:w-[25rem] lg:w-[22rem] w-[20rem] bg-slate-200 rounded-3xl shadow-xl`}
+              className={`flex flex-col justify-between gap-3 xl:w-[22rem] lg:w-[22rem] w-[20rem] bg-slate-200 rounded-3xl shadow-xl`}
             >
               <div className="flex flex-col p-5 justify-around gap-5">
                 {/* breadcrumbs */}
@@ -44,9 +74,9 @@ const CTODevInsights = () => {
               <img
                 src={insight.img}
                 alt={insight.title}
-                className="imgColorChange object-cover h-[14rem] xl:w-[24rem] lg:w-[22rem] w-[20rem] bg-slate-100 bg-opacity-60 rounded-3xl"
+                className="imgColorChange object-cover h-[14rem] w-full bg-slate-100 bg-opacity-60 rounded-3xl"
               />
-            </div>
+            </motion.div>
           );
         })}
       </div>
